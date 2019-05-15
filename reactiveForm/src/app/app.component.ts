50ae01172b10028a8a5fe1f80b1f4777b46d6b71
sachin <sachin@ahex.co.in>
2019-05-15 19:14:51 +0530

    custom validator

---
+++ reactiveForm/src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { forbiddenNameValidator } from './shared/user-name.validators';
+// import { FormGroup, FormControl } from "@angular/forms";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+
+export class AppComponent {
+  get userName(){
+  return this.registrationForm.get('userName')
+}
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(){
+    // this.registrationForm.setValue({
+    //   patchValue for few formcontrol field
+    //   userName: 'Sachin',
+    //   password:  'test',
+    //   confirmPassword: 'test',
+    //   address: {
+    //     city: 'City',
+    //     state: 'State',
+    //     pinCode: '250002'
+    //   }
+    // })
+  }
+
+
+  registrationForm=this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(3),forbiddenNameValidator]],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city:[''],
+      state:[''],
+      pinCode:['']
+    })
+  })
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl(''),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //   city: new FormControl(''),
+  //   state: new FormControl(''),
+  //   pinCode: new FormControl('')
+  //   })
+  // });
+}
